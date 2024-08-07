7352fbb3f802bd22c93397f91a075debabd14734
Jana Mališić <72757013+janamalisic@users.noreply.github.com>
2024-08-07 10:23:43 +0200

    kontakt push

--- sendemail.php
+++ sendemail.php
@@ -8,16 +8,10 @@ require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
-if (isset($_POST['name'], $_POST['email'], $_POST['message'])) {
-    $name = trim($_POST['name']);
-    $email = trim($_POST['email']);
-    $message = trim($_POST['message']);
-
-    // Dodatna validacija na serverskoj strani
-    if (empty($name) || empty($email) || empty($message)) {
-        echo 'Molimo popunite sva polja.';
-        exit;
-    }
+if (isset($_POST['submitContact'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
     $mail = new PHPMailer(true);
 
@@ -30,26 +24,33 @@ if (isset($_POST['name'], $_POST['email'], $_POST['message'])) {
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;
 
-        $mail->setFrom('janaijoca2003@gmail.com', 'Funda of Web IT');
-        $mail->addAddress('janaijoca2003@gmail.com', 'Funda of Web IT');
+        $mail->setFrom('janaijoca2003@gmail.com', 'Squadra');
+        $mail->addAddress('janaijoca2003@gmail.com', 'Squadra');
 
         $mail->isHTML(true);
-        $mail->Subject = 'Nova poruka - Funda of web it kontakt forma';
-        $bodyContent = '<div>Zdravo, dobili ste novu poruku</div>
-            <div>Ime: '.$name.'</div>
-            <div>Email: '.$email.'</div>
-            <div>Poruka: '.$message.'</div>';
-        $mail->Body = $bodyContent; 
-        
-        if($mail->send()) {
-            echo 'Vaša poruka je uspešno poslata!';
+        $mail->Subject = 'New enquiry - Squadra Contact Form';
+
+        $bodyContent = '<div>Hello, you got a new enquiry</div>
+            <div>Ime: ' . $name . '</div>
+            <div>Email: ' . $email . '</div>
+            <div>Message: ' . $message . '</div>';
+
+        $mail->Body = $bodyContent;
+
+        if ($mail->send()) {
+            $_SESSION['status'] = "Thank you for contacting us - Team Squadra Perfetta";
+            header("Location: " . $_SERVER['HTTP_REFERER']);
+            exit(0);
         } else {
-            echo 'Greška prilikom slanja poruke. Molimo pokušajte ponovo.';
+            $_SESSION['status'] = "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            header("Location: " . $_SERVER['HTTP_REFERER']);
+            exit(0);
         }
     } catch (Exception $e) {
         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
 } else {
-    echo 'Molimo popunite sva polja.';
+    header('Location: index.html');
+    exit(0);
 }
 ?>
